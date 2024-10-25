8b85e0f39a6e013079a63b261e84283164415d47
Eldos <eldos.baktybekov@gmail.com>
2024-10-26 00:04:04 +0600

    :sparkles: Add reply markup builder

--- pages/mailing/reply-markup.vue
+++ pages/mailing/reply-markup.vue
@@ -3,21 +3,31 @@
     <ReplyMarkupBuilder
       v-model:buttons="buttons"
     />
-    <MainButton @click="onSubmit"/>
+    <MainButton
+      @click="onSubmit"
+      text="Сохранить"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import ReplyMarkupBuilder from '~/components/forms/ReplyMarkupBuilder.vue'
-import { MainButton, useWebApp } from 'vue-tg'
+import { MainButton, useWebApp, useWebAppPopup } from 'vue-tg'
 import type { Button } from '~/reply-markup'
 
+const { showAlert } = useWebAppPopup()
+const { sendData } = useWebApp()
 const buttons = ref<Button[]>([])
 
-const { sendData } = useWebApp()
+const anyButton = computed((): boolean => buttons.value.length > 0)
 
 const onSubmit = () => {
-  sendData?.(JSON.stringify(buttons.value))
+  if (!anyButton.value) {
+    showAlert?.('Добавьте хотя бы одну кнопку')
+  } else {
+    sendData?.(JSON.stringify({ event: 'attach_reply_markup', buttons: buttons.value }))
+  }
 }
+
 
 </script>
