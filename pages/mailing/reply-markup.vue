eb824275aab84140af47a0fcd8785633e8d972a6
Eldos <eldos.baktybekov@gmail.com>
2024-10-25 23:07:52 +0600

    :tada: Initial

---
+++ pages/mailing/reply-markup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ReplyMarkupBuilder/>
+    <MainButton @click="onSubmit"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ReplyMarkupBuilder from '~/components/forms/ReplyMarkupBuilder.vue'
+import { MainButton, useWebApp } from 'vue-tg'
+
+const { sendData } = useWebApp()
+
+const onSubmit = () => {
+  sendData?.(JSON.stringify({ hello: 'world' }))
+}
+
+interface Button {
+  text: string
+  url: string
+}
+
+type ButtonsRow = Button[]
+type ButtonsRows = ButtonsRow[]
+</script>
