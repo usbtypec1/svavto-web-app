9ed9091401a3cad8174922bd1e62a130507f83ff
Eldos <eldos.baktybekov@gmail.com>
2024-11-02 11:06:44 +0600

    :sparkles: Change staff available dates pick page

--- pages/shifts/confirm.vue
+++ pages/shifts/confirm.vue
@@ -99,6 +99,7 @@ const url = `${runtimeConfig.public.apiBaseUrl}/shifts/staff/`
 
 const { data, execute, status } = useFetch(url, {
   query: { date: formattedDate },
+  immediate: false,
 })
 
 const staffList = computed(() => {
