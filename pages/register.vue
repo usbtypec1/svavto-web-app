fbda4a585041c1846dd51f18f2611ab8403bd1b5
Eldos <eldos.baktybekov@gmail.com>
2024-11-06 11:56:53 +0600

    :sparkles: Change staff available dates pick page

--- pages/register.vue
+++ pages/register.vue
@@ -1,59 +1,100 @@
 <template>
   <div>
     <Fieldset legend="Регистрация">
-      <form @submit.prevent class="flex flex-col gap-y-4">
-        <div class="flex flex-col gap-y-2">
-          <label class="font-semibold">ФИО</label>
-          <InputText v-model.trim="fullName"/>
+      <Form
+        v-slot="$form"
+        :initialValues
+        :resolver
+        class="flex flex-col gap-y-4"
+        @submit="onFormSubmit"
+      >
+        <div class="flex flex-col gap-1">
+          <label for="fullName">ФИО</label>
+          <InputText id="fullName" name="fullName" type="text" placeholder="Пупкин Василий Иванович" fluid/>
+          <Message v-if="$form.fullName?.invalid" severity="error" size="small" variant="simple">
+            {{ $form.fullName.error?.message }}
+          </Message>
         </div>
-        <div class="flex flex-col gap-y-2">
-          <label>Номер телефона, привязанный к аккаунту в каршеринге</label>
-          <InputText v-model.trim="carSharingPhoneNumber"/>
+        <div class="flex flex-col gap-1">
+          <label for="carSharingPhoneNumber">Номер телефона, привязанный к аккаунту в каршеринге</label>
+          <InputMask id="carSharingPhoneNumber" name="carSharingPhoneNumber" mask="+7 (999) 999-99-99"
+                     placeholder="+7 (351) 240-04-40" fluid/>
+          <Message v-if="$form.carSharingPhoneNumber?.invalid" severity="error" size="small" variant="simple">
+            {{ $form.carSharingPhoneNumber.error?.message }}
+          </Message>
         </div>
-        <div class="flex flex-col gap-y-2">
-          <label>Номер телефона, указанный в компании Консоль</label>
-          <InputText v-model.trim="consolePhoneNumber"/>
+        <div class="flex flex-col gap-1">
+          <label for="consolePhoneNumber">Номер телефона, указанный в компании Консоль</label>
+          <InputMask
+            id="consolePhoneNumber"
+            name="consolePhoneNumber"
+            mask="+7 (999) 999-99-99"
+            placeholder="+7 (351) 240-04-40"
+            fluid
+          />
+          <Message v-if="$form.consolePhoneNumber?.invalid" severity="error" size="small" variant="simple">
+            {{ $form.consolePhoneNumber.error?.message }}
+          </Message>
         </div>
-      </form>
+        <DevOnly>
+          <Button
+            type="submit"
+            label="Зарегистрироваться"
+            :disabled="!$form.valid"
+          />
+        </DevOnly>
+        <MainButton
+          type="submit"
+          text="Зарегистрироваться"
+          :visible="$form.valid"
+        />
+      </Form>
     </Fieldset>
-    <MainButton
-      @click="onConfirm"
-      text="Зарегистрироваться"
-    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { MainButton, useWebApp, useWebAppPopup } from 'vue-tg'
+import { Form } from '@primevue/forms'
+import { MainButton } from 'vue-tg'
+import { zodResolver } from '@primevue/forms/resolvers/zod'
+import { z } from 'zod';
+import { useWebApp, useWebAppHapticFeedback } from 'vue-tg'
 
-const { showAlert } = useWebAppPopup()
+
+const { notificationOccurred } = useWebAppHapticFeedback()
 const { sendData } = useWebApp()
 
-const fullName = ref<string>('')
-const carSharingPhoneNumber = ref<string>('')
-const consolePhoneNumber = ref<string>('')
+const phoneNumberRegExp = new RegExp('^\\+7\\s\\(\\d{3}\\)\\s\\d{3}-\\d{2}-\\d{2}$')
 
-const isAllFieldsFilled = computed((): boolean => {
-  return (
-    fullName.value.length > 0
-    && carSharingPhoneNumber.value.length > 0
-    && consolePhoneNumber.value.length > 0
-  )
-})
 
-const serializedData = computed((): string => {
-  return JSON.stringify({
-    full_name: fullName.value,
-    car_sharing_phone_number: carSharingPhoneNumber.value,
-    console_phone_number: consolePhoneNumber.value,
-  })
-})
+const resolver = ref(zodResolver(
+  z.object({
+    fullName: z.string()
+      .min(5, { message: 'Минимальная длина ФИО - 5 символов' })
+      .max(64, { message: 'Максимальная длина ФИО - 64 символов' }),
+    carSharingPhoneNumber: z.string()
+      .min(1, { message: 'Введите номер телефона' })
+      .regex(phoneNumberRegExp, { message: 'Неверный формат номера телефона' }),
+    consolePhoneNumber: z.string()
+      .min(1, { message: 'Введите номер телефона' })
+      .regex(phoneNumberRegExp, { message: 'Неверный формат номера телефона' }),
+  }),
+))
 
-const onConfirm = (): void => {
-  if (isAllFieldsFilled.value) {
-    sendData?.(serializedData.value)
-  } else {
-    showAlert?.('Вы должны заполнить все поля')
-  }
+const initialValues = ref({
+  fullName: '',
+  carSharingPhoneNumber: '',
+  consolePhoneNumber: '',
+});
+
+
+const onFormSubmit = ({ valid, values }) => {
+  if (!valid) return
+  notificationOccurred?.('success')
+  sendData?.(JSON.stringify({
+    full_name: values.fullName,
+    car_sharing_phone_number: values.carSharingPhoneNumber,
+    console_phone_number: values.consolePhoneNumber,
+  }))
 }
 </script>
