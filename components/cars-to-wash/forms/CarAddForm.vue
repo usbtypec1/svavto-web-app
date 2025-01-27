14c04edd18403579071a8779de973fd6e1e94f59
Eldos <eldos.baktybekov@gmail.com>
2025-01-27 11:33:44 +0600

    :sparkles: Update windshield washer refilled bottle percentage options to include additional values for improved accuracy

--- components/cars-to-wash/forms/CarAddForm.vue
+++ components/cars-to-wash/forms/CarAddForm.vue
@@ -183,7 +183,7 @@ const resolver = ref(zodResolver(
   }),
 ))
 
-const windshieldWasherRefilledBottlePercentageOptions: number[] = [10, 20, 30, 50, 70, 90, 100, 120]
+const windshieldWasherRefilledBottlePercentageOptions: number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120]
 
 const onSubmit = ({ valid, values, originalEvent }: FormSubmitEvent) => {
   if (!valid) {
