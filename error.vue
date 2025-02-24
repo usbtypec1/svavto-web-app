5913323cc512ef3b304e08919f2cc95f0da5a176
Eldos <eldos.baktybekov@gmail.com>
2025-02-25 00:57:43 +0600

    Add error handling component for page load failures

---
+++ error.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout>
+    <Message severity="error" icon="pi pi-exclamation-triangle">
+      Произошла ошибка при загрузке страницы
+    </Message>
+  </NuxtLayout>
+</template>
