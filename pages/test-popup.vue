8b18f388b325c4c91658a0bb682473b4130a2a38
Eldos <eldos.baktybekov@gmail.com>
2025-03-06 11:36:15 +0600

    ✨ Add test-popup component with text input and alert functionality

---
+++ pages/test-popup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Textarea v-model="text" />
+    <p>Длина: {{ text.length }}</p>
+    <Button label="Показать" @click="showAlert(text)" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useWebAppPopup } from "vue-tg"
+
+const text = ref<string>("")
+
+const { showAlert } = useWebAppPopup()
+</script>
