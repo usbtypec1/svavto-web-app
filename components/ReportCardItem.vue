9519edb3674bd5d8a7351411516d96bfd7e3af32
Eldos <eldos.baktybekov@gmail.com>
2025-04-21 19:34:35 +0600

    ✨ Update ReportCardItem component to use date-fns for date formatting and remove redundant date conversion functions

--- components/ReportCardItem.vue
+++ components/ReportCardItem.vue
@@ -1,7 +1,7 @@
 <template>
   <Card>
     <template #title>
-      Смена {{ dateToDDMMYYYY(new Date(shiftStatistics.shift_date)) }}
+      Смена {{ format(parseISO(shiftStatistics.date), "dd.MM.yyyy") }}
       <template v-if="shiftStatistics.is_extra_shift">(доп.смена)</template>
     </template>
     <template #content>
@@ -12,19 +12,9 @@
 
 <script setup lang="ts">
 import type { ShiftStatistics } from "~/types/reports"
+import { format, parseISO } from "date-fns"
 
 defineProps<{
   shiftStatistics: ShiftStatistics
 }>()
-
-const dateToYYYYMMDD = (date?: Date): string | undefined => {
-  if (date === undefined) return
-  return date.toISOString().split("T")[0]
-}
-
-const dateToDDMMYYYY = (date?: Date): string | undefined => {
-  if (date === undefined) return
-  const [year, month, day] = dateToYYYYMMDD(date)!.split("-")
-  return `${day}.${month}.${year}`
-}
 </script>
