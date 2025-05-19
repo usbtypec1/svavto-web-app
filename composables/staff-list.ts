b8fc67d6f6b9354fbb7abb8509e4d5af2f32a290
Eldos <eldos.baktybekov@gmail.com>
2025-05-20 00:33:50 +0600

    ✨ Update useStaffList to use useApiFetch and improve type handling in transform function

--- composables/staff-list.ts
+++ composables/staff-list.ts
@@ -5,11 +5,11 @@ type OrderOption = "-created_at" | "full_name"
 export const useStaffList = (
   { orderBy }: { orderBy: OrderOption } = { orderBy: "-created_at" },
 ) => {
-  return useApiFetch<StaffIdAndName[]>("/staff/", {
+  return useApi<StaffIdAndName[]>("/staff/", {
     query: {
       order_by: orderBy,
     },
-    transform(data: { staff: Staff[] }): StaffIdAndName[] {
+    transform(data: any): StaffIdAndName[] {
       return data.staff
         .filter((staff: Staff): boolean => staff.banned_at === null)
         .map((staff: Staff) => ({
